b1d8d7e60672efe8c4adb61833fff798d31c738e
Sumonpal <95920466+Sumonpal@users.noreply.github.com>
2024-05-08 20:15:35 +0600

    Add files via upload

---
+++ New folder/Reflection/Reflection/Initializer1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Initializer1
+    {
+        public void InitStartup()
+        {
+            Console.WriteLine("Starting Initializer in setup 1");
+        }
+    }
+}
---
+++ New folder/Reflection/Reflection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"F:\file\.Net Code\Reflection\Reflection\config.txt";
+            var configText = File.ReadAllText(path);
+
+            var initClassName = configText.Split('=')[1].Trim();
+
+            Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types) 
+            { 
+                if(type.Name == initClassName)
+                {
+                    var constructor = type.GetConstructor(new Type[0]);
+                    var initializerInstance = constructor.Invoke(new object[0]);
+
+                    MethodInfo method = type.GetMethod("InitStartup");
+                    method.Invoke(initializerInstance, new object[0]);
+                }
+            }
+        }
+    }
+}
